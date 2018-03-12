f509bef72c775bd34293d502fd837f9c6fb7633b
aimotee <1846919406@qq.com>
2018-03-13 00:43:49 +0800

    update
    shop

--- program/application/index/controller/Index.php
+++ program/application/index/controller/Index.php
@@ -1,8 +1,8 @@
 <?php
 namespace app\index\controller;
-use think\console\command\make\Model;
 use think\Controller;
 use think\Db;
+use think\Request;
 class Index extends Controller
 {
     public function index2()
@@ -50,6 +50,17 @@ class Index extends Controller
 
     public function add()
     {
+        $request = Request::instance();
+        echo '请求方法：' . $request->method() . '<br/>';
+        echo '资源类型：' . $request->type() . '<br/>';
+        echo '访问ip地址：' . $request->ip() . '<br/>';
+        echo '是否AJax请求：' . var_export($request->isAjax(), true) . '<br/>';
+        echo '请求参数：';
+        dump($request->post());
+        echo Request::instance()->post('name');
+        echo "<br/>";
+        echo input('post.comment');
+
         return $this->fetch();
     }
 
@@ -69,6 +80,11 @@ class Index extends Controller
             $this->error('提交失败','add');
         }
 
+
+    }
+
+    public function dele()
+    {
 
     }
 
